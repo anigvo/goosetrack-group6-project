d2d798ec6d7126cb832f384724df808ad09d2424
anigvo <116902287+anigvo@users.noreply.github.com>
2023-09-28 14:55:35 +0000

    Deploying to gh-pages from @ anigvo/goosetrack-group6-project@601c0a6b0359a25bead3e4499f2d0229f65324ce 🚀

---
+++ static/js/65.67d0b28b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[65],{65:function(e,t,r){r.r(t);var h=r(184);t.default=function(){return(0,h.jsx)("h1",{children:"ChoosedMonth"})}}}]);
+//# sourceMappingURL=65.67d0b28b.chunk.js.map
--- static/js/816.53fe064b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[816],{5816:function(e,a,n){n.r(a);var r=n(2791),t=n(7689),s=n(184);a.default=function(e){var a=e.updatePageName;return(0,r.useEffect)((function(){a("Calendar")}),[a]),(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)("h1",{children:"Calendar page"}),(0,s.jsx)(r.Suspense,{children:(0,s.jsx)(t.j3,{})})]})}}}]);
-//# sourceMappingURL=816.53fe064b.chunk.js.map
