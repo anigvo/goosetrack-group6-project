4be4b807e908835bbbfdba04094b9b9d6ce32004
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-09-30 17:19:03 +0300

    add sample tasks to style

---
+++ src/layout/ChoosedDay/temp_example.js
@@ -0,0 +1,52 @@
+export const exampleTasks = {
+    todo: [{
+        id: '1',
+        text: 'Complete project report',
+        priority: 'high'
+    },
+    {
+        id: '2',
+        text: 'Organize team meeting',
+        priority: 'medium'
+    },
+    {
+        id: '3',
+        text: 'Respond to client emails',
+        priority: 'low'
+    },
+    {
+        id: '4',
+        text: 'Review and revise presentation slides because it is very important to make',
+        priority: 'medium'
+    }],
+    inprogress: [{
+        id: '5',
+        text: 'Research new market trends',
+        priority: 'high'
+    },
+    {
+        id: '6',
+        text: 'Prepare agenda for upcoming meeting',
+        priority: 'low'
+    },
+    {
+        id: '7',
+        text: 'Follow up with potential leads',
+        priority: 'medium'
+    },
+    {
+        id: '8',
+        text: 'Organize office workspace',
+        priority: 'low'
+    }],
+    done: [{
+        id: '9',
+        text: 'Attend training workshop',
+        priority: 'high'
+    },
+    {
+        id: '10',
+        text: 'Create content for social media',
+        priority: 'medium'
+    }]
+}
