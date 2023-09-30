e565fb903bb002c0170c1dce7f0a6b2e0915a5bd
VVervolk <113511730+VVervolk@users.noreply.github.com>
2023-09-30 17:32:56 +0300

    complete page

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,81 +1,83 @@
 export const lightTheme = Object.freeze({
-    mainFont: '#111111',
-    mainBackground: '#F7F6F9',
-    // кольори сайдбару
-    sidebarBackground: '#FFFFFF',
-    sidebarText: 'rgba(52, 52, 52, 0.5)',
-    sidebarTextOutline: '#3E85F3',
-    sidebarTextBackgroundOutline: '#E3F3FF',
-    logoTextColor: '#3E85F3',
-    // фон для всіх сторінок з mainlayout
-    oddBackground: '#FFFFFF',
-    // кольори сторінки юзера
-    userAvatarBorder: '#3E85F3',
-    userAvatarInsideBackground: '#F8F8F8',
-    userNameTextColor: '#343434',
-    userInputLabelColor: '#111111',
-    userInputBorderColor: 'rgba(17, 17, 17, 0.15)',
-    userInputTextColor: '#111111',
-    // кольори хедера
-    headerUsernameText: '#343434',
-    themeTogglerIcon: '#3E85F3',
-    // кольори сторінки статистики
-    byDayColor: '#FFD2DD',
-    byMonthsColor: '#3E85F3',
-    statisticsTextColor: '#343434',
-    arrowDisabledColor: '#DCE3E5',
-    arrowEnabledColor: '#343434',
-    chartBorderColor: '#E3F3FF',
-    // кольори сторінки календаря
-    calendarBackground: '#ffffff',
-    calendarBorderColor: 'rgba(220, 227, 229, 0.80)',
-    calendarTextColor: '#343434',
-    calendarLowTaskBackground: '#CEEEFD',
-    calendarLowTaskText: '#3E85F3',
-    calendarMediumTaskBackground: '#FCF0D4',
-    calendarMediumTaskText: '#F3B249',
-    calendarHighTaskBackground: '#FFD2DD',
-    calendarHighTaskText: '#EA3D65',
-    // кнопки переключення між сторінками calendar/month calendar/day
-    periodTypeBackground: '#E3F3FF',
-    periodTypeText: '#3E85F3',
-    periodTypeTextOutline: '#3E85F3',
-    periodTypeBackgroundOutline: '#CAE8FF',
-    periodTypeBorderline: '#3E85F333',
-    // сторінка calendar/day
-    taskBackground: '#F7F6F9',
-    taskBorderColor: 'rgba(220, 227, 229, 0.8)',
-    taskPriorityTextColor: '#F7F6F9',
-    taskIconColor: '#111111',
-    taskActiveIconColor: '#3E85F3',
-    taskScrollBarBackfround: '#F2F2F2',
-    // пріорітетність тасків
-    taskLowPriority: '#72C2F8',
-    taskMediumPriority: '#F3B249',
-    taskHighPriority: '#EA3D65',
-    // кнопка додавання таски, для кнопки текст mainFont
-    addTaskBtnBackground: '#E3F3FF',
-    addTaskBtnBorder: '#3E85F3',
-    // модалки для рейтингу та додавання тасків
-    modalBackground: '#FFF',
-    modalBorder: 'rgba(220, 227, 229, 0.80)',
-    modalActiveStarColor: '#FFAC33',
-    modalInactiveStarColor: '#CEC9C1',
-    modalInputLabelColor: 'rgba(52, 52, 52, 0.80)',
-    modalInputBackground: '#F6F6F6',
-    modalEditIconColor: '#3E85F3',
-    modalEditActiveIconColor: '#FFFFFF',
-    modalEditActiveIconBackground: '#3E85F3',
-    modalEditIconBackground: '#E3F3FF',
-    modalDeleteIconColor: '#EA3D65',
-    modalDeleteIconBackground: 'rgba(234, 61, 101, 0.20)',
-    // кнопки скасування різні для фідбеку та тасків
-    modalCancelBtnFeedbackBackground: '#E5EDFA',
-    modalCancelBtnTaskBackground: '#EFEFEF',
-    // кольори кнопок (однакові на темній та світлій темах)
-    btnBackgroundColor: '#3E85F3',
-    btnTextColor: '#FFFFFF',
-    btnHoverBackground: '#2B78EF'
+  mainFont: '#111111',
+  mainBackground: '#F7F6F9',
+  // кольори сайдбару
+  sidebarBackground: '#FFFFFF',
+  sidebarText: 'rgba(52, 52, 52, 0.5)',
+  sidebarTextOutline: '#3E85F3',
+  sidebarTextBackgroundOutline: '#E3F3FF',
+  logoTextColor: '#3E85F3',
+  // фон для всіх сторінок з mainlayout
+  oddBackground: '#FFFFFF',
+  // кольори сторінки юзера
+  userAvatarBorder: '#3E85F3',
+  userAvatarInsideBackground: '#F8F8F8',
+  userNameTextColor: '#343434',
+  userInputLabelColor: '#111111',
+  userInputBorderColor: 'rgba(17, 17, 17, 0.15)',
+  userInputTextColor: '#111111',
+  // кольори хедера
+  headerUsernameText: '#343434',
+  themeTogglerIcon: '#3E85F3',
+  // кольори сторінки статистики
+  byDayColor: '#FFD2DD',
+  byMonthsColor: '#3E85F3',
+  statisticsTextColor: '#343434',
+  arrowDisabledColor: '#DCE3E5',
+  arrowEnabledColor: '#343434',
+  chartBorderColor: '#E3F3FF',
+  // кольори сторінки календаря
+  calendarBackground: '#ffffff',
+  calendarBorderColor: 'rgba(220, 227, 229, 0.80)',
+  calendarTextColor: '#343434',
+  calendarLowTaskBackground: '#CEEEFD',
+  calendarLowTaskText: '#3E85F3',
+  calendarMediumTaskBackground: '#FCF0D4',
+  calendarMediumTaskText: '#F3B249',
+  calendarHighTaskBackground: '#FFD2DD',
+  calendarHighTaskText: '#EA3D65',
+  // кнопки переключення між сторінками calendar/month calendar/day
+  periodTypeBackground: '#E3F3FF',
+  periodTypeText: '#3E85F3',
+  periodTypeTextOutline: '#3E85F3',
+  periodTypeBackgroundOutline: '#CAE8FF',
+  periodTypeBorderline: '#3E85F333',
+  // сторінка calendar/day
+  taskBackground: '#F7F6F9',
+  taskBorderColor: 'rgba(220, 227, 229, 0.8)',
+  taskPriorityTextColor: '#F7F6F9',
+  taskIconColor: '#111111',
+  taskActiveIconColor: '#3E85F3',
+  taskScrollBarBackfround: '#F2F2F2',
+  // пріорітетність тасків
+  taskLowPriority: '#72C2F8',
+  taskMediumPriority: '#F3B249',
+  taskHighPriority: '#EA3D65',
+  // кнопка додавання таски, для кнопки текст mainFont
+  addTaskBtnBackground: '#E3F3FF',
+  addTaskBtnBorder: '#3E85F3',
+  // модалки для рейтингу та додавання тасків
+  modalBackground: '#FFF',
+  modalBorder: 'rgba(220, 227, 229, 0.80)',
+  modalActiveStarColor: '#FFAC33',
+  modalInactiveStarColor: '#CEC9C1',
+  modalInputLabelColor: 'rgba(52, 52, 52, 0.80)',
+  modalInputBackground: '#F6F6F6',
+  modalEditIconColor: '#3E85F3',
+  modalEditActiveIconColor: '#FFFFFF',
+  modalEditActiveIconBackground: '#3E85F3',
+  modalEditIconBackground: '#E3F3FF',
+  modalDeleteIconColor: '#EA3D65',
+  modalDeleteIconBackground: 'rgba(234, 61, 101, 0.20)',
+  // кнопки скасування різні для фідбеку та тасків
+  modalCancelBtnFeedbackBackground: '#E5EDFA',
+  modalCancelBtnTaskBackground: '#EFEFEF',
+  // кольори кнопок (однакові на темній та світлій темах)
+  btnBackgroundColor: '#3E85F3',
+  btnTextColor: '#FFFFFF',
+  btnHoverBackground: '#2B78EF',
+  // колір тексту на 404
+  apologizeTextColor: 'rgba(17, 17, 17, 0.698)',
 });
 
 export const darkTheme = Object.freeze({
@@ -155,4 +157,6 @@ export const darkTheme = Object.freeze({
   btnBackgroundColor: '#3E85F3',
   btnTextColor: '#FFFFFF',
   btnHoverBackground: '#2B78EF',
+  // колір тексту на 404
+  apologizeTextColor: '#FFFFFF',
 });
