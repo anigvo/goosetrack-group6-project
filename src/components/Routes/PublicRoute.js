45dea77ee1d80a5238f03bfc9e79d24dd3b9ee42
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-10-02 17:40:04 +0300

    add public and private route components

---
+++ src/components/Routes/PublicRoute.js
@@ -0,0 +1,7 @@
+import { useAuth } from "hooks/useAuth";
+import { Navigate } from "react-router-dom";
+
+export const PublicRoute = ({ component: Component, redirectTo = '/' }) => {
+  const {isLoggedIn} = useAuth();
+  return isLoggedIn ? <Navigate to={redirectTo}/> : Component;
+};
