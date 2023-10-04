00fe032cad30d6b7a874b98da08f0dc8991bde6f
GumeniukRuslan <bishob1997@gmail.com>
2023-10-04 14:47:34 +0300

    minor

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,7 +2,6 @@ import AuthSection from "components/AuthSection/AuthSection";
 import Description from "components/Description/Description";
 import ReviewSlider from "components/ReviewsSlider/ReviewsSlider";
 import { MainPageContainer, MainPageWrapper } from "./MainPage.styled";
-// import './mainpage.css';
 
 const MainPage = () => {
     return (
